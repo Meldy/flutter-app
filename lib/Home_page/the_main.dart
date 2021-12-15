775a6622cb890e35f19c2dd055990c550443d1e0
Emelda <emativenga16@outlook.com>
2021-12-15 12:05:01 +0200

    Initial commit

---
+++ lib/Home_page/the_main.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() => runApp(const MyMap());
+
+class MyMap extends StatefulWidget {
+  const MyMap({Key? key}) : super(key: key);
+
+  //final GlobalKey<ScaffoldState>_drawerscaffoldKey = new GlobalKey <ScaffoldState> ();
+
+  @override
+  _MyMapState createState() => _MyMapState();
+}
+//my api key is AIzaSyBC1EPXfpwVZr8XTP90xB5LN51w98qYdAM
+/*class _MyMapState extends State<MyMap> with AutomaticKeepAliveClientMixin {
+  late GoogleMapController mapController;
+
+  @override
+  bool get wantKeepAlive => true;*/
+class _MyMapState extends State<MyMap>{
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+        title: const Text('my map'),
+           backgroundColor: Colors.blue,
+        ),
+           /*leading: IconButton(onPressed: () { setState(() {
+              Scaffold(
+                  drawer: Drawer(
+                    child: ListView(
+                      padding: EdgeInsets.zero,
+                      children: [
+                        const DrawerHeader(
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                          ),
+                          child: Text('Smart city traveller'),
+                        ),
+                        ListTile(
+                          title: const Text('My location'),
+                          onTap: () {
+                            // Update the state of the app.
+                            // ...
+                            Navigator.pop(context);
+                          },
+                        ),
+                        ListTile(
+                          title: const Text('Get directions'),
+                          onTap: () {
+                            // Update the state of the app.
+                            // ...
+                            Navigator.pop(context);
+                          },
+                        ),
+                        ListTile(
+                          title: const Text('another item'),
+                          onTap: () {
+                            // Update the state of the app
+                            // ...
+                            // Then close the drawer
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
+                    ),
+                  ));
+            });
+            }, icon: const Icon(Icons.menu),),
+            title: const Text('Sample Map'),
+            actions: const [
+              //Icon(Icons.favorite),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Icon(Icons.search),
+              ),
+              //Icon(Icons.more_vert),
+            ],
+            backgroundColor: Colors.lightBlue,
+          ),*/
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
---
+++ lib/main.dart
@@ -0,0 +1,144 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_signin_button/button_builder.dart';
+import 'package:smart_city_traveller/register_page.dart';
+import 'package:smart_city_traveller/signin_page.dart';
+import 'package:smart_city_traveller/Home_page/the_main.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _firebaseApp = Firebase.initializeApp();
+
+  MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Sign in',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.lightBlue,
+      ),
+      home: FutureBuilder(
+        future: _firebaseApp,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text('You have an error ${snapshot.error.toString()}');
+          } else if (snapshot.hasData) {
+            return const MyHomePage(title: 'Flutter Demo Home Page');
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  /*
+    https://blog.logrocket.com/implementing-firebase-authentication-in-a-flutter-app/
+    https://medium.com/firebase-developers/dive-into-firebase-auth-on-flutter-phone-and-anonymous-authentication-6ce4f17eb2a8
+   */
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  final TextEditingController _phoneNumberController = TextEditingController();
+  final TextEditingController _smsController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Smart city traveller',
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        body: AuthTypeSelector(),
+      ),
+    );
+  }
+}
+
+/// Provides a UI to select a authentication type page
+class AuthTypeSelector extends StatelessWidget {
+  // Navigates to a new page
+  void _pushPage(BuildContext context, Widget page) {
+    Navigator.of(context) /*!*/ .push(
+      MaterialPageRoute<void>(builder: (_) => page),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Smart city traveller'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(16),
+            alignment: Alignment.center,
+            child: SignInButtonBuilder(
+              icon: Icons.person_add,
+              backgroundColor: Colors.indigo,
+              text: 'Registration',
+              onPressed: () => _pushPage(context, RegisterPage()),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(16),
+            alignment: Alignment.center,
+            child: SignInButtonBuilder(
+              icon: Icons.verified_user,
+              backgroundColor: Colors.orange,
+              text: 'Sign In',
+              //onPressed: () => _pushPage(context, SignInPage()), redirects to the signin page which is being worked on
+              onPressed: () => _pushPage(context, const MyMap()),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
